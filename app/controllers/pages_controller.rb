b203e44df76925fc82e21d15844220d39531f30e
Meghan Martin <meghanxmartin@gmail.com>
2020-03-16 13:56:12 +0100

    creates stock lookup search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @api = StockQuote::Stock.new(api_key: 'pk_825aa235840141d69333256db3cff8dc')
+    if params[:search].present?
+      begin
+        @stock = StockQuote::Stock.quote(params[:search])
+      rescue
+        @error = "That symbol doesn't exist. Search again."
+      end
+    end
   end
 end
